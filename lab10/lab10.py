2f6ed107ec324178a8386f8a7e89777154449169
limravac <69934560+limravac@users.noreply.github.com>
2021-05-03 20:34:28 -0500

    lab 10

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -15,6 +15,9 @@ class AVLTree:
 
         def rotate_left(self):
             ### BEGIN SOLUTION
+            n = self.right
+            self.val, n.val = n.val, self.val
+            self.right, n.right, self.left, n.left = n.right, n.left, n, self.left
             ### END SOLUTION
 
         @staticmethod
@@ -31,16 +34,80 @@ class AVLTree:
     @staticmethod
     def rebalance(t):
         ### BEGIN SOLUTION
+        rightSubTree = AVLTree.Node.height(t.right)
+        leftSubTree = AVLTree.Node.height(t.left)
+        balanceFactor = rightSubTree - leftSubTree
+        if balanceFactor <= -2:
+            #if the balance factor is -2 the AVL tree is left heavy
+            smallerRight = AVLTree.Node.height(t.left.right)
+            smallerLeft = AVLTree.Node.height(t.left.left)
+            if smallerRight > smallerLeft:
+                #if the right subtree is longer then we are in LR and need to rotate subnodes left first
+                t.left.rotate_left()
+            t.rotate_right() #this happens no matter what, LR or LL
+        elif balanceFactor >= 2:
+            #This means we are right heavy
+            smallerRight = AVLTree.Node.height(t.right.right)
+            smallerLeft = AVLTree.Node.height(t.right.left)
+            if smallerLeft > smallerRight:
+                #If left subtree is longer then we have an RL situation and need to rotate subnodes right first
+                t.right.rotate_right()
+            t.rotate_left() #happens no matter what, RL or RR
+        
         ### END SOLUTION
 
     def add(self, val):
         assert(val not in self)
         ### BEGIN SOLUTION
+        def add_rec(node):
+            if not node:
+                #the spot where the node should go is empty, so we insert here
+                return AVLTree.Node(val)
+            elif val < node.val:
+                node.left = add_rec(node.left) #continue recursion
+            elif val > node.val:
+                node.right = add_rec(node.right) #continue recursion
+            AVLTree.rebalance(node) #check the balance and rotate nodes as needed
+            return node
+        self.root = add_rec(self.root)
+        self.size += 1
         ### END SOLUTION
 
+    def tmax(root):
+        if not root.right:
+            return root.val
+        else:
+            return AVLTree.tmax(root.right)
+
     def __delitem__(self, val):
+        #print("VAL: " + str(val))
         assert(val in self)
         ### BEGIN SOLUTION
+        def del_rec(node, value):
+            if value < node.val:  #if value less than current node's value
+                node.left = del_rec(node.left, value)   #assign the left node to the recursive deletion of the left node with the same value
+            elif value > node.val:  #same as above but right
+                node.right = del_rec(node.right, value)
+            elif value == node.val:   #if the value is equal to that of the current node
+                if not node.left and not node.right: #has no children, simply delete normally.
+                    return None
+                elif not node.left:       
+                    return node.right   #if it does not have a left child, replace the previous node that we're deleting (node) with its right child
+                elif not node.right:    #or if it doesnt have a right child, assign its left child to it
+                    return node.left
+                #if we hit this point then it has two kids, so find largest child in left subtree and assign its val to cur node, then delete it.
+                current = node.left
+                while current.right:
+                    current = current.right
+                node.val = current.val
+                node.left = del_rec(node.left, node.val)
+            AVLTree.rebalance(node)
+            return node
+
+        self.root = del_rec(self.root, val)
+        self.size -= 1
+            #self.pprint()   
+            
         ### END SOLUTION
 
     def __contains__(self, val):
@@ -121,7 +188,7 @@ def test_ll_fix_simple():
 
     for x in [3, 2, 1]:
         t.add(x)
-
+        
     tc.assertEqual(height(t.root), 2)
     tc.assertEqual([t.root.left.val, t.root.val, t.root.right.val], [1, 2, 3])
 
@@ -133,7 +200,7 @@ def test_rr_fix_simple():
 
     for x in [1, 2, 3]:
         t.add(x)
-
+        #t.pprint()
     tc.assertEqual(height(t.root), 2)
     tc.assertEqual([t.root.left.val, t.root.val, t.root.right.val], [1, 2, 3])
 
@@ -145,6 +212,7 @@ def test_lr_fix_simple():
 
     for x in [3, 1, 2]:
         t.add(x)
+        t.pprint()
 
     tc.assertEqual(height(t.root), 2)
     tc.assertEqual([t.root.left.val, t.root.val, t.root.right.val], [1, 2, 3])
@@ -179,6 +247,7 @@ def test_key_order_after_ops():
     vals.sort()
 
     for i,val in enumerate(t):
+        print([val, vals[i]])
         tc.assertEqual(val, vals[i])
 
 # stress testing
@@ -222,6 +291,13 @@ def say_success():
 # MAIN
 ################################################################################
 def main():
+    a = AVLTree()
+
+    for x in [3, 2, 1]:
+        a.add(x)
+        a.pprint()
+    del a[1]
+    a.pprint()
     for t in [test_ll_fix_simple,
               test_rr_fix_simple,
               test_lr_fix_simple,
