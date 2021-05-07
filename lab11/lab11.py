ada8f1df21325436539fe86d99040468726816c1
limravac <69934560+limravac@users.noreply.github.com>
2021-05-06 20:01:23 -0500

    lab 11 done

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -1,25 +1,71 @@
 from unittest import TestCase
 import random
 
+pivots = []
+
 def quicksort(lst,pivot_fn):
     qsort(lst,0,len(lst) - 1,pivot_fn)
 
 def qsort(lst,low,high,pivot_fn):
     ### BEGIN SOLUTION
+    if low < high:
+        p = hoare(lst, low, high, pivot_fn)
+        qsort(lst, low, p, pivot_fn)
+        qsort(lst, p + 1, high, pivot_fn)
+        
     ### END SOLUTION
 
 def pivot_first(lst,low,high):
     ### BEGIN SOLUTION
+    return lst[low]
     ### END SOLUTION
 
 def pivot_random(lst,low,high):
     ### BEGIN SOLUTION
+    return lst[random.randrange(low, high)]
     ### END SOLUTION
 
 def pivot_median_of_three(lst,low,high):
     ### BEGIN SOLUTION
+    numA = lst[low]
+    numB = lst[(low + high) // 2]
+    numC = lst[high]
+    l = [numA, numB, numC]
+    minl = min(l)
+    maxl = max(l)
+    i = 0
+    for a in l:
+        i += 1
+        if a == minl or a == maxl:
+            del l[i]
+            i -= 1
+    return l[0]
+    
     ### END SOLUTION
+def hoare(lst, low, high, pivot_fn):
+    ppos = pivot_fn(lst, low, high)
+    pivot = lst[ppos]
+    swap(lst, ppos, low) #moving the pivot to the very front so that this partitioning can work if it isnt min pivot
+    i = low
+    j = high
+    while i < j:
+        #moving i and j in closer together
+        #print("PIVOT: " + str(pivot))
+        #print(lst)
+        while lst[i] < pivot and i < j:
+            i += 1
+            #print("i: " + str(i) + " and j: " + str(j))
+        while lst[j] > pivot and i < j:
+            j -= 1
+            #print("i: " + str(i) + " and j: " + str(j))
+        swap(lst, i, j) #swap if i is still less than j
+    swap(lst, low, j) #move the pivot into its proper position when i >= j
+    return j    #return j As per instructions
 
+def swap(lst, i, j):
+    temp = lst[i] 
+    lst[i] = lst[j]
+    lst[j] = temp
 ################################################################################
 # TEST CASES
 ################################################################################
